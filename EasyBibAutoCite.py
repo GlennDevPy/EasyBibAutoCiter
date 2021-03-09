a63375fe6c9dcdf0bfcb47b39b14f49a2075e918
GlennDev <glenn.lee.23@seoulforeign.org>
2021-03-09 18:06:12 +0900

    small edits and goals

--- EasyBibAutoCite.py
+++ EasyBibAutoCite.py
@@ -179,7 +179,7 @@ def singleL(l1):
             authorL = input()
 
         if "publishedDayFound" in check:
-            print("Published Day? (YY)")
+            print("Published Day? (DD)")
             dD = input()
 
         if "publishedMonthFound" in check:
@@ -238,7 +238,10 @@ def singleL(l1):
     #cSC = s.post(cP, headers=headersCP, data=json.dumps(data2))
     #print(cSC.text)
     dM = dM.replace("0", "")
-    paragraph = worddoc.add_paragraph(f'{authorL}, {authorF}. "{title}." {webtitle}, {dD} {calendar.month_abbr[int(dM)]}. {dY}, {l1}.')
+    if dM == '':
+        paragraph = worddoc.add_paragraph(f'{authorL}, {authorF}. "{title}." {webtitle}, {dD}. {dY}, {l1}.')
+    else:    
+        paragraph = worddoc.add_paragraph(f'{authorL}, {authorF}. "{title}." {webtitle}, {dD} {calendar.month_abbr[int(dM)]}. {dY}, {l1}.')
     paragraph.paragraph_format.left_indent = Inches(0.5)
     paragraph.paragraph_format.first_line_indent = Inches(-0.5)
     worddoc.save(f'{strftime("%Y-%m-%d %H.%M.%S", localtime())} Citations.docx')
@@ -260,7 +263,7 @@ def textfile(tpath):
             links.append(i.rstrip())
     for letsgo in links:
         multilink = f"https://autocite.citation-api.com/api/v3/query?url={letsgo}"
-        singler = s.get(multilink, headers=headersMain, timeout=3)
+        singler = s.get(multilink, headers=headersMain, timeout=10)
         try:
             score = re.search('score\":(.*?),', singler.text)[1]
         except:
@@ -377,7 +380,7 @@ def textfile(tpath):
                 authorL = input()
 
             if "publishedDayFound" in check:
-                print("Published Day? (YY)")
+                print("Published Day? (DD)")
                 dD = input()
 
             if "publishedMonthFound" in check:
@@ -472,9 +475,13 @@ def textfile(tpath):
             #values 3 = original authors
             if values2 in values3.values():
                 final.append(values3)
+    #todo if author blank etc
     for outputs in final:
         outputs['dM'] = outputs['dM'].replace("0", "")
-        paragraph = worddoc.add_paragraph(f"{outputs['authorL']}, {outputs['authorF']}. \"{outputs['title']}.\" {outputs['webtitle']}, {outputs['dD']} {calendar.month_abbr[int(outputs['dM'])]}. {outputs['dY']}, {outputs['furl']}.")
+        if outputs['dM'] == '':
+            paragraph = worddoc.add_paragraph(f"{outputs['authorL']}, {outputs['authorF']}. \"{outputs['title']}.\" {outputs['webtitle']}, {outputs['dD']}. {outputs['dY']}, {outputs['furl']}.")
+        else:
+            paragraph = worddoc.add_paragraph(f"{outputs['authorL']}, {outputs['authorF']}. \"{outputs['title']}.\" {outputs['webtitle']}, {outputs['dD']} {calendar.month_abbr[int(outputs['dM'])]}. {outputs['dY']}, {outputs['furl']}.")
         paragraph.paragraph_format.left_indent = Inches(0.5)
         paragraph.paragraph_format.first_line_indent = Inches(-0.5)
     worddoc.save(f'{strftime("%Y-%m-%d %H.%M.%S", localtime())} Citations.docx')
@@ -515,7 +522,7 @@ def manual():
             break
     for letsgo in urls:
         multilink = f"https://autocite.citation-api.com/api/v3/query?url={letsgo}"
-        singler = s.get(multilink, headers=headersMain, timeout=3)
+        singler = s.get(multilink, headers=headersMain, timeout=10)
         try:
             score = re.search('score\":(.*?),', singler.text)[1]
         except:
@@ -727,7 +734,242 @@ def manual():
     #get the citations
     #dump this into word doc
 
+def noexppath():
+    os.system('cls')
+    print("EZBibCiter [beta] by GlennDev\n")
+    allcites = []
+    urls = []
+    while True:
+        print(f"Current URL List:\n")
+        for index, item in enumerate(urls, start=1):
+            print(f"{index} | {item}")
+        print(f'\n[1] Add\n[2] Remove\n[3] Done')
+        selectxxx3 = input()
+        if selectxxx3 == "1":
+            print("Enter URL to Add:\n")
+            urladd = input()
+            urls.append(urladd.rstrip())
+            os.system('cls')
+        if selectxxx3 == "2":
+            print("Which URL Index to Remove? (1, 2, 3, etc)")
+            indexrem = input()
+            indexrem = int(indexrem)-1
+            urls.pop(indexrem)
+        if selectxxx3 == "3":
+            break
+    for letsgo in urls:
+        multilink = f"https://autocite.citation-api.com/api/v3/query?url={letsgo}"
+        singler = s.get(multilink, headers=headersMain, timeout=10)
+        try:
+            score = re.search('score\":(.*?),', singler.text)[1]
+        except:
+            score = 'Missing'
 
+        try:
+            title2 = re.search('title\":\"(.*?)\"', singler.text)[1]
+            title = codecs.decode(title2, 'unicode-escape')
+        except:
+            title = 'Missing'
+
+        try:
+            webtitle = re.search('_mediumTitle\":\"(.*?)\"', singler.text)[1]
+        except:
+            webtitle = 'Missing'
+
+        try:
+            authorF = re.search('given\":\"(.*?)\"', singler.text)[1]
+        except:
+            authorF = 'Missing'
+
+        try:
+            authorL = re.search('family\":\"(.*?)\"', singler.text)[1]
+        except:
+            authorL = 'Missing'
+
+
+        dD = re.search('_publishedDay\":\"(.*?)\"', singler.text)[1]
+        if dD == '':
+            dD = "Missing"
+
+
+        dM = re.search('_publishedMonth\":\"(.*?)\"', singler.text)[1]
+        if dM == '':
+            dM = "Missing"
+
+
+        dY = re.search('_publishedYear\":\"(.*?)\"', singler.text)[1]
+        if dY == '':
+            dY = "Missing"
+        
+
+        checklist = {
+            "title": title,
+            "webtitle": webtitle,
+            "authorF": authorF,
+            "authorL": authorL,
+            "dD": dD,
+            
+            "dM": dM,
+            "dY": dY,
+            'furl': letsgo
+        }
+
+        tocheck = ['mediumTitleFound', 'contentTitleFound', 'contributorsFound', 'publishedDayFound', 'publishedMonthFound', 'publishedYearFound']
+        
+        #medtitle is website title
+        if score == "100":
+            print(f"\nCurrent Citation Progress:\n{checklist}\n")
+            print("Do you want to make any edits? (Y or N)")
+            cedits = input()
+            if cedits.upper() == "Y":
+                while True:
+                    print(f"URL: {letsgo}")
+                    print("\nTo which section?\n[1] Title\n[2] Website Title\n[3] Author First\n[4] Author Last\n[5] Day Pub\n[6] Month Pub\n[7] Year Pub\n[8] Done")
+                    sectionedit = input()
+                    if sectionedit == "1":
+                        print(f'Change title from [{title}] to what?')
+                        title = input()
+                    if sectionedit == "2":
+                        print(f'Change website title from [{webtitle}] to what?')
+                        webtitle = input()
+                    if sectionedit == "3":
+                        print(f'Change author first name from [{authorF}] to what?')
+                        authorF = input()
+                    if sectionedit == "4":
+                        print(f'Change author last name from [{authorL}] to what?')
+                        authorL = input()
+                    if sectionedit == "5":
+                        print(f'Change day published to what?')
+                        dD = input()
+                    if sectionedit == "6":
+                        print(f'Change month published to what?')
+                        dM = input()
+                    if sectionedit == "7":
+                        print(f'Change year published to what?')
+                        dY = input()
+                    if sectionedit == "8":
+                        break
+            else:
+                pass
+        else:
+            check = []
+            for plz in tocheck:
+                xPlz = re.search(f'{plz}\":(.*?),\"', singler.text)[1]
+                if xPlz == "0":
+                    check.append(plz)
+                else:
+                    pass
+            
+            print(f"\nCurrent Citation Progress:\n{checklist}\n")
+            if "mediumTitleFound" in check:
+                print("Website Title?")
+                webtitle = input()
+
+            if "contentTitleFound" in check:
+                print("Article Title?")
+                title = input()
+
+            if "contributorsFound" in check:
+                print("Author First Name?")
+                authorF = input()
+
+                print("Authors Last Name?")
+                authorL = input()
+
+            if "publishedDayFound" in check:
+                print("Published Day? (YY)")
+                dD = input()
+
+            if "publishedMonthFound" in check:
+                print("Published Month? (MM)")
+                dM = input()
+                
+            if "publishedYearFound" in check:
+                print("Published Year? (YYYY)")
+                dY = input()
+            
+            checklist = {
+            "title": title,
+            "webtitle": webtitle,
+            "authorF": authorF,
+            "authorL": authorL,
+            "dD": dD,
+            "dM": dM,
+            "dY": dY,
+            'furl': letsgo
+            }
+
+            print(f"\nUpdated Citations:\n{checklist}\n")
+            print("Do you want to make any edits? (Y or N)")
+            cedits = input()
+            if cedits.upper() == "Y":
+                while True:
+                    print(f"URL: {letsgo}")
+                    print("\nTo which section?\n[1] Title\n[2] Website Title\n[3] Author First\n[4] Author Last\n[5] Day Pub\n[6] Month Pub\n[7] Year Pub\n[8] Done")
+                    sectionedit = input()
+                    if sectionedit == "1":
+                        print('Change title to what?')
+                        title = input()
+                    if sectionedit == "2":
+                        print('Change website title to what?')
+                        webtitle = input()
+                    if sectionedit == "3":
+                        print('Change author first name to what?')
+                        authorF = input()
+                    if sectionedit == "4":
+                        print('Change author last name to what?')
+                        authorL = input()
+                    if sectionedit == "5":
+                        print('Change day published to what?')
+                        dD = input()
+                    if sectionedit == "6":
+                        print('Change month published to what?')
+                        dM = input()
+                    if sectionedit == "7":
+                        print('Change year published to what?')
+                        dY = input()
+                    if sectionedit == "8":
+                        break
+            else:
+                pass
+        checklist = {
+            "title": title,
+            "webtitle": webtitle,
+            "authorF": authorF,
+            "authorL": authorL,
+            "dD": dD,
+            "dM": dM,
+            "dY": dY,
+            'furl': letsgo
+        }
+        allcites.append(checklist)
+        #print(f"\nUpdated Citations:\n{checklist}\n")
+        #data2 = {"operationName":"CreateCitation","variables":{"projectId":projID,"contributors":[{"function":"author","first":authorF,"middle":"","last":authorL,"data":{"suffix":""}}],"annotation":"","data":{"source":"website","pubtype":{"main":"pubonline","suffix":""},"website":{"title":title},"pubonline":{"title":webtitle,"inst":"","url":letsgo,"day":dD,"month":dM,"year":dY,"timestamp":""},"annotation":"","validatorStatus":"complete"}},"query":"mutation CreateCitation($projectId: String!, $pubType: String, $sourceType: String, $annotation: String, $contributors: [ContributorInput!], $data: JSON!) {\n  createCitation(projectId: $projectId, pubType: $pubType, sourceType: $sourceType, annotation: $annotation, contributors: $contributors, data: $data) {\n    citationId\n    id: citationId\n    data\n    annotation\n    __typename\n  }\n}\n"}
+        #cSC = s.post(cP, headers=headersCP, data=json.dumps(data2))
+    sort = []
+    final = []
+    for values in allcites:
+        sort.append(values["authorL"])
+    sort.sort()
+    for values2 in sort:
+        #values 2 = sorted authors
+        for values3 in allcites:
+            #values 3 = original authors
+            if values2 in values3.values():
+                final.append(values3)
+    for outputs in final:
+        outputs['dM'] = outputs['dM'].replace("0", "")
+        paragraph = worddoc.add_paragraph(f"{outputs['authorL']}, {outputs['authorF']}. \"{outputs['title']}.\" {outputs['webtitle']}, {outputs['dD']} {calendar.month_abbr[int(outputs['dM'])]}. {outputs['dY']}, {outputs['furl']}.")
+        paragraph.paragraph_format.left_indent = Inches(0.5)
+        paragraph.paragraph_format.first_line_indent = Inches(-0.5)
+    worddoc.save(f'{strftime("%Y-%m-%d %H.%M.%S", localtime())} Citations.docx')
+    print(f"\nFinished Citations - File at {strftime('%Y-%m-%d %H.%M.%S', localtime())} Citations.docx")
+    
+    #data3 = {"operationName":"Citationcount","variables":{"projectId":projID},"query":"query Citationcount($projectId: String!) {\n  citationCount(projectId: $projectId) {\n    count\n    __typename\n  }\n}\n"}
+    #cx = s.post(cP, headers=headersCP, data=json.dumps(data3))
+    #print(cx.text)
+    #get the citations
+    #dump this into word doc  
 
 
 print("EZBibCiter [beta] by GlennDev\n")
@@ -748,3 +990,5 @@ if choice == "2":
 if choice == "3":
     manual()
     
+if choice == "4":
+    noexppath()
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# EasyBibAutoCiter
-Automatically cites from easybib using api and requests - no need for ads 
+### EasyBibAutoCiter
 
-3 Modes
+## Automatically cites from easybib using api and requests - no need for ads 
+
+# 3 Modes
+
 1. Cite from text file
 - WORKING! Drag in text file and will auto cite
 2. Cite one thing
@@ -11,6 +13,11 @@ Automatically cites from easybib using api and requests - no need for ads
 
 Program will take links, and automatically cite them using easybib api. If its not 100% good (ie. fields missing), then will prompt user to fill those in. It is extremely messy and probably could use iteration but works for now. User can then edit if easybib did a bad job. 
 
-Yet to do:
+### Yet to do:
+
+
+
+PRIORITY - ADD A WAY TO EXPORT FROM LINK AND GO STRAIGHT TO PROJECT PAGE**
+
 Bug fixes if any
 Cleaning up Code
